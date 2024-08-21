a9f02bd2459e9e103ac8daa306d6cf9cf5358ef9
Björn Erlwein <bjoern@bjoernerlwein.de>
2024-08-21 19:00:46 +0200

    Initial commit

---
+++ no-for-in.test.ts
@@ -0,0 +1,37 @@
+import {RuleTester} from "@typescript-eslint/rule-tester";
+import rule from "./no-for-in.js";
+import test from "node:test";
+
+RuleTester.afterAll = test.after;
+RuleTester.describe = test.describe;
+RuleTester.it = test.it;
+
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    ecmaVersion: 2015
+  }
+});  
+
+
+ruleTester.run(
+  "no-for-in",
+  rule,
+  {
+    valid: [
+      {
+	code: "for (x of y) {}"
+      }
+    ],
+    invalid: [
+      {
+	code: "for (const x in y) {}",
+	errors: [{messageId: 'no-for-in'}]
+      },
+      {
+	code: "for (let x in []) {}",
+	errors: [{messageId: 'no-for-in'}]
+      },
+    ]
+  }
+)
---
+++ no-for-in.ts
@@ -0,0 +1,32 @@
+import {ESLintUtils} from "@typescript-eslint/utils";
+
+const createRule = ESLintUtils.RuleCreator(
+  name => `berlwein/${name}`
+);
+
+const rule = createRule({
+  create(context) {
+    return {
+      ForInStatement(node) {
+	context.report({
+	  messageId: 'no-for-in',
+	  node: node
+	})
+      }
+    }
+  },
+  name: "no-for-in",
+  meta: {
+    docs: {
+      description: 'For..in loops iterate over enumerable string properties. This is most likely uninteded'
+    },
+    type: "problem",
+    messages: {
+      "no-for-in": "Use for..of instead"
+    },
+    schema: []
+  },
+  defaultOptions: []
+})
+
+export default rule;
