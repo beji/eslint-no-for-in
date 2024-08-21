5bab2e44161434cf51e8a6c70aa2fc7f275bfede
Björn Erlwein <bjoern@bjoernerlwein.de>
2024-08-21 19:32:39 +0200

    add missing index file

---
+++ index.ts
@@ -0,0 +1,6 @@
+import {TSESLint} from "@typescript-eslint/utils";
+import noForIn from "./no-for-in";
+
+export const rules = {
+  'no-for-in': noForIn
+} satisfies Record<string, TSESLint.RuleModule<string, Array<unknown>>>;
